e14a5e9de4537e0f529274eb7d53a3c1669274b6
yizik <syizikm@gmail.com>
2026-04-15 10:37:07 +0300

    mobile fix

---
+++ maze-game/src/app/const/grid.ts
@@ -0,0 +1,13 @@
+export const COLS_AND_ROWS = {
+    cols: 20,
+    rows: 20
+};
+
+export const CELL_SIZES: Record<string, number> = {
+    'gan': 56,
+    '1-2': 48,
+    '3-4': 40,
+    '5-6': 32,
+};
+
+export const FRUIT_COUNTS = 8;
